1a0a87ef53f28a7fd3a52d3a750263e28115d6f1
bnz236 <132112294+bnz236@users.noreply.github.com>
2024-11-14 02:35:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '4915678382282'
+global.nomerowner = ["4915678382282"]
 
 // Apikey 
 
